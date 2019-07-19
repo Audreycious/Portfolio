4bdb37300bf86b82e9fe6b3feb6866702defd3f2
Audreycious <porcupine4@gmail.com>
2019-07-18 23:20:57 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,52 @@
+
+const STORE = {
+    homePageBG: null,
+    aboutPageContent: null,
+    projectsPageContent: null,
+    heroPageContent: null
+};
+
+
+function buttonHandler(params) {
+    // listen for when clicked
+    // click trigger bounces to the appropriate section
+    // about button
+    $("body").on('click', "")
+    // home button
+    $("body").on('click', "")
+    // input button
+    $("body").on('click', "")
+    // projects button
+    $("body").on('click', "")
+    // email button
+    $("body").on('click', "")
+    // phone number button
+    $("body").on('click', "")
+    // hamburger menu button
+        // changes background to blurred version
+    $("body").on('click', "")
+
+    // stretch: add a contact button that causes a pop up over the hero page and changes the background to the blurred version
+    
+}
+
+function textFade(params) {
+    // fade the hero text in from left to right when the home page loads
+    // return
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function viewHandler(params) {
+    // being mindful for future development where I can have all the views load dynamically when the buttons are pressed. Would remove the bounce down button at that time.
+}
