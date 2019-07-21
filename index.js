38847d73d4e4d1aaa5127525661d8f1c8f0f16d4
Audreycious <porcupine4@gmail.com>
2019-07-21 00:53:28 -0500

    nav bar done

--- index.js
+++ index.js
@@ -12,21 +12,34 @@ function buttonHandler(params) {
         // click trigger bounces to the appropriate section
     // home button
     $("body").on('click', ".home-button")
-        // 
+        // trigger the home view in the rendering area when either the logo or the hom button are clicked
 
     // about button
-    $("body").on('click', "#about-button")
+    $("body").on('click', ".about-button")
+        // trigger the about view when about is clicked
+
     // projects button
-    $("body").on('click', "#projects-button")
+    $("body").on('click', ".projects-button")
+        // trigger the projects view when projects is clicked
+
     // contact button - stretch
-    $("body").on('click', "#contact-button")
+    $("body").on('click', ".contact-button")
+        // trigger the contact popup when contact is clicked
+
     // email button
-    $("body").on('click', "#email-button")
+    $("body").on('click', ".email-button")
+        // trigger the the creation of a new email when email hyperlink is clicked
+            // add a subject of "I'm interested in your services"
+
     // phone number button
-    $("body").on('click', "#phone-button")
+    $("body").on('click', ".phone-button")
+        // trigger the calling with the set up calling service when the phone number is clicked
+            // stretch - if no set up calling service, ask to send it to your phone as a text
+
     // hamburger menu button
         // changes background to blurred version
-    $("body").on('click', "#hamburger-button")
+    $("body").on('click', ".hamburger-button")
+        // trigger the hamburger menu popup with new hyper links that use the buttons above
 
     // stretch: add a contact button that causes a pop up over the hero page and changes the background to the blurred version
     
