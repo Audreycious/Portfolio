8f1ab8c8fe2e71db4d1a4c5cd433eaa880a8bc4d
Audreycious <porcupine4@gmail.com>
2019-07-28 05:24:36 -0500

    end of saturday night

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const STORE = {
     homePageBG: null,
     aboutPageContent: null,
     projectsPageContent: null,
-    heroPageContent: null
+    heroPageContent: null,
+    hamburgerMenuOpen: false
 };
 
 
@@ -11,34 +12,54 @@ function buttonHandler(params) {
     // listen for when clicked
         // click trigger bounces to the appropriate section
     // home button
-    $("body").on('click', ".home-button")
+    $("main").on('click', ".home-button", function() {
+        $("header").html(`
+            <div role="hero-text" class="hero-text">
+                <h1>Audrey Foss</h1>
+                <p>Future, expert, full-stack Web Developer based in Minneapolis, MN. 
+                Specializing in creative content design, unique strategies, and superb customer service.</p>
+            </div>
+        `)
+    })
         // trigger the home view in the rendering area when either the logo or the hom button are clicked
 
     // about button
-    $("body").on('click', ".about-button")
+    // $("body").on('click', ".about-button")
         // trigger the about view when about is clicked
 
     // projects button
-    $("body").on('click', ".projects-button")
+    // $("body").on('click', ".projects-button")
         // trigger the projects view when projects is clicked
 
     // contact button - stretch
-    $("body").on('click', ".contact-button")
+    // $("body").on('click', ".contact-button")
         // trigger the contact popup when contact is clicked
 
     // email button
-    $("body").on('click', ".email-button")
+    // $("body").on('click', ".email-button")
         // trigger the the creation of a new email when email hyperlink is clicked
             // add a subject of "I'm interested in your services"
 
     // phone number button
-    $("body").on('click', ".phone-button")
+    // $("body").on('click', ".phone-button")
         // trigger the calling with the set up calling service when the phone number is clicked
             // stretch - if no set up calling service, ask to send it to your phone as a text
 
     // hemburgairrrrr menu button
         // changes background to blurred version
-    $("body").on('click', ".hemburgairrrrr-button")
+    $("main").on('click', "input.hemburgairrrrr-menu-button", function () {
+        console.log("Hamburger clicked!");
+        STORE.hamburgerMenuOpen = !STORE.hamburgerMenuOpen;
+        if (STORE.hamburgerMenuOpen) {
+            $("#hemburgairrrrr-content-box").fadeIn("slow", function() {
+
+        })}
+        else {
+            $("#hemburgairrrrr-content-box").fadeOut("slow", function() {
+
+            })
+        }
+    })
         // trigger the hemburgairrrrr menu popup with new hyper links that use the buttons above
 
     // stretch: add a contact button that causes a pop up over the hero page and changes the background to the blurred version
@@ -56,12 +77,9 @@ function textFade(params) {
 
 
 
-
-
-
-
-
-
 function viewHandler(params) {
     // being mindful for future development where I can have all the views load dynamically when the buttons are pressed. Would remove the bounce down button at that time.
 }
+
+
+buttonHandler();
